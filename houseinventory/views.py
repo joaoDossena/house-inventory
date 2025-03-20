b122f609767e89e3ca557a0e6b2213e8ad1c5602
joaoDossena <joaopdossena@gmail.com>
2025-03-20 19:39:10 -0300

    Feature: Templates, listing articles from database

--- houseinventory/views.py
+++ houseinventory/views.py
@@ -1,11 +1,23 @@
 
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from articles.models import Article
+from random import randint
 
 def home_view(request):
-    
-    name = "poggers"
-    HTML_STRING = f"""<h1>Hello {name}!</h1>"""
+    random_id = randint(1, 3)
+    print(f"randint: {random_id}")
+    article_obj = Article.objects.get(id=random_id)
+    article_qs = Article.objects.all()
+
+    context = {
+        "object_list": article_qs,
+        "id": article_obj.id,
+        "title": article_obj.title,
+        "content": article_obj.content,
+    }
+
+    HTML_STRING = render_to_string("home-view.html", context=context)
 
     return HttpResponse(HTML_STRING)
