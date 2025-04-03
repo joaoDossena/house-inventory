b3e3503905871d8af94dabac907819f347970e8b
joaoDossena <joaopdossena@gmail.com>
2025-04-03 16:03:58 -0300

    Fix: Removed print of username and password

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        print(username, password)
         user = authenticate(request, username=username, password=password)
         if user is None:
             context = {"error": "Invalid username or password"}
