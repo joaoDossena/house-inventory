24bf6db0a24c77bed2ae83a6dce74634296da05d
joaoDossena <joaopdossena@gmail.com>
2025-04-03 10:31:37 -0300

    Feature: Added primitive search and added a way of creating articles

--- articles/views.py
+++ articles/views.py
@@ -2,14 +2,41 @@ from django.shortcuts import render
 
 from .models import Article
 
-# Create your views here.
+def article_search_view(request):
+    query_dict = request.GET
+    article_obj = None
+
+    try:
+        query = int(query_dict.get("q"))
+    except:
+        query = None
+    
+
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+
+    context = {
+        "object": article_obj
+    }
+    return render(request, "articles/search.html", context=context)
+
+def article_create_view(request):
+    context = {}
+    
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        article_object = Article.objects.create(title=title, content=content)
+        context['object'] = article_object
+        context['created'] = True
+        
+    return render(request, "articles/create.html", context=context)
+
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
         article_obj = Article.objects.get(id=id)
-
-    print(article_obj)
-
     context = {
         "object": article_obj,
     }
