888931f3910a46f88efd84d73b25abcd73b3195b
joaoDossena <joaopdossena@gmail.com>
2025-04-08 11:33:19 -0300

    Feature: Added Django model forms for article creation, and added a method to clean data that checks for article titles already in use

--- articles/views.py
+++ articles/views.py
@@ -31,9 +31,7 @@ def article_create_view(request):
     }
     
     if form.is_valid():
-        title = form.cleaned_data.get("title")
-        content = form.cleaned_data.get("content")
-        article_object = Article.objects.create(title=title, content=content)
+        article_object = form.save()
         context['object'] = article_object
         context['created'] = True
         
