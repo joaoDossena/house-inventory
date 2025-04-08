48ca5f1a3918838ec3c5a38b835dc31df3e40fbd
joaoDossena <joaopdossena@gmail.com>
2025-04-08 12:21:42 -0300

    refactor: change manual login to Django AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request, "accounts/login.html", context=context)
-        login(request, user)
-        return redirect("/")
-    return render(request, "accounts/login.html", context={})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm(request)
+    context = {"form": form}
+    return render(request, "accounts/login.html", context=context)
 
 def logout_view(request):
     if request.method == "POST":
