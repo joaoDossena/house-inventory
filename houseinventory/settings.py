8ec69f76cf361f7e9f43a5dfb9cea11c9ee37277
joaoDossena <joaopdossena@gmail.com>
2025-04-09 10:31:42 -0300

    build: add .env file for sensitive environment variables that should not be in production

--- houseinventory/settings.py
+++ houseinventory/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+7^ez9#&k3=l-tsc!-eeyhjiy1j%-)l3x$o$qz55ls^ehd)&l#'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+7^ez9#&k3=l-tsc!-eeyhjiy1j%-)l3x$o$qz55ls^ehd)&l#')
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
+print(DEBUG)
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
