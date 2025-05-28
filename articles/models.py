539b45c27efab630fd51d48c4cb467555b3e9509
joaoDossena <joaopdossena@gmail.com>
2025-05-28 17:56:57 -0300

    feat(articles/models.py): add primitive slugification of articles

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-# from django.utils import timezone
+from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
+
 
 # Create your models here.
 class Article(models.Model):
@@ -12,6 +13,19 @@ class Article(models.Model):
     publish = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    print("pre_save")
+    if instance is None:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(article_pre_save, sender=Article)
+
+def article_post_save(sender, instance, created, *args, **kwargs):
+    print("post_save")
+    if created:
+        instance.slug = slugify(instance.title)
+        instance.save()
+
+post_save.connect(article_post_save, sender=Article)
